c9889e365e481f846c1de3195c83b7298a369724
YOUR_NAME <YOUR_EMAIL>
2025-02-06 12:56:45 -0600

    Coded the bottle song.

--- bottles.py
+++ bottles.py
@@ -1,6 +1,23 @@
+"""
+- take note of how bottles changes to bottle when single digits
+- use for loop first, then try to make simpler/prettier
+"""
+"""""
+- need to start at 99
+- print each iteration with one less
+- use for i in range(start, stop, step)
+- use string interpolation : f"{ i } bottles of beer on the wall, { i } bottles of beer. Take one down and pass it around, { i } bottles of beer on the wall."
+
+"""""
+
 def bottle_song():
-	# write your code here!
-	pass
+	for i in range(99, -1, -1):
+		if i > 9:
+			print (f"{i} bottles of beer on the wall, {i} bottles of beer. Take one down and pass it around, {i} bottles of beer on the wall.")
+		elif i <= 9 and i > 0:
+			print (f"{i} bottle of beer on the wall, {i} bottle of beer. Take one down and pass it around, {i} bottle of beer on the wall.")
+		elif i == 0: 
+			print("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall!")
 
 bottle_song()
 
